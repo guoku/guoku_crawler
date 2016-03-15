4e41ff262e53c6f6740c5fb635902ed04783d5da
goalong <goforalong@gmail.com>
2016-03-15 13:20:23 +0800

    use or_

--- guoku_crawler/article/crawler.py
+++ guoku_crawler/article/crawler.py
@@ -6,13 +6,14 @@ from guoku_crawler.article.rss import crawl_rss_list
 from guoku_crawler.models import CoreGkuser, AuthGroup
 from guoku_crawler.article.weixin import crawl_weixin_list
 from guoku_crawler.models import CoreAuthorizedUserProfile as Profile
+from sqlalchemy import or_
 
 
 @app.task(base=RequestsTask, name='weixin.crawl_articles')
 def crawl_articles():
     users = session.query(CoreGkuser).filter(
-        CoreGkuser.authorized_profile.any(
-            Profile.weixin_id.isnot(None) or Profile.rss_url.isnot(None)),
+        CoreGkuser.authorized_profile.any(or_(
+            Profile.weixin_id.isnot(None), Profile.rss_url.isnot( None))),
         CoreGkuser.groups.any(AuthGroup.name == 'Author')
     ).all()
     for user in users:
@@ -28,3 +29,4 @@ def crawl_user_articles(authorized_user_id):
         crawl_rss_list.delay(authorized_user_id)
     else:
         crawl_weixin_list.delay(authorized_user_id)
+
